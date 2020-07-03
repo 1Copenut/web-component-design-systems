0069b99c6b2e98c4278e4d1732c31fa7bd054935
1Copenut <pierce.trevor@gmail.com>
2020-07-03 12:30:24 -0500

    WIP. Refactoring tests for class and instance separation.

--- open-web-components/components/continuum-accordion/test/continuum-accordion.test.js
+++ open-web-components/components/continuum-accordion/test/continuum-accordion.test.js
@@ -11,30 +11,13 @@ import '../continuum-accordion.js';
 import { ContinuumAccordion } from '../src/ContinuumAccordion.js';
 
 describe('ContinuumAccordion', () => {
-  it('Renders the correct HTML', async () => {
-    const el = await fixture(html`
-      <continuum-accordion></continuum-accordion>
-    `);
-    const button = el.shadowRoot.querySelector('button');
-    const container = el.shadowRoot.querySelector('div[hidden]');
-    const heading = el.shadowRoot.querySelector('h2');
+  it('CLASS: Creates a new basic instance', async () => {
+    const el = new ContinuumAccordion();
 
-    expect(button).to.exist;
-    expect(container).to.exist;
-    expect(heading).to.exist;
+    expect(el).to.exist;
   });
 
-  it('Includes the correct ARIA markup', async () => {
-    const el = await fixture(html`
-      <continuum-accordion></continuum-accordion>
-    `);
-    const button = el.shadowRoot.querySelector('button');
-
-    expect(button).to.have.attribute('aria-expanded');
-    expect(button.getAttribute('aria-expanded')).to.equal('false');
-  });
-
-  it('Calls validateProperties method on render', async () => {
+  it('CLASS: Calls validateProperties method on render', async () => {
     let checkProps = false;
     const tag = defineCE(
       class extends ContinuumAccordion {
@@ -48,23 +31,13 @@ describe('ContinuumAccordion', () => {
     expect(checkProps).to.be.true;
   });
 
-  it('Sets open attribute to "false"', async () => {
-    const el = await fixture(html`
-      <continuum-accordion></continuum-accordion>
-    `);
+  it('CLASS: Sets open attribute to "false"', async () => {
+    const el = new ContinuumAccordion();
 
     expect(el.open).to.equal('false');
   });
 
-  it('Allows user to override open attribute', async () => {
-    const el = await fixture(html`
-      <continuum-accordion open="true"></continuum-accordion>
-    `);
-
-    expect(el.open).to.equal('true');
-  });
-
-  it('Throws an error on invalid open attribute', async () => {
+  it('CLASS: Throws an error on invalid open attribute', async () => {
     const el = new ContinuumAccordion('falsy');
 
     expect(() => el.validateProperties()).to.throw(
@@ -72,7 +45,38 @@ describe('ContinuumAccordion', () => {
     );
   });
 
-  it('Passes the a11y audit', async () => {
+  it('INSTANCE: Renders the correct HTML', async () => {
+    const el = await fixture(html`
+      <continuum-accordion></continuum-accordion>
+    `);
+    const button = el.shadowRoot.querySelector('button');
+    const container = el.shadowRoot.querySelector('div[hidden]');
+    const heading = el.shadowRoot.querySelector('h2');
+
+    expect(button).to.exist;
+    expect(container).to.exist;
+    expect(heading).to.exist;
+  });
+
+  it('INSTANCE: Includes the correct ARIA markup', async () => {
+    const el = await fixture(html`
+      <continuum-accordion></continuum-accordion>
+    `);
+    const button = el.shadowRoot.querySelector('button');
+
+    expect(button).to.have.attribute('aria-expanded');
+    expect(button.getAttribute('aria-expanded')).to.equal('false');
+  });
+
+  it('INSTANCE: Allows user to override open attribute', async () => {
+    const el = await fixture(html`
+      <continuum-accordion open="true"></continuum-accordion>
+    `);
+
+    expect(el.open).to.equal('true');
+  });
+
+  it('INSTANCE: Passes the a11y audit', async () => {
     const el = await fixture(html`
       <continuum-accordion></continuum-accordion>
     `);
